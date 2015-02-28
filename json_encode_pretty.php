956c5377ade67c273ddcd8d67844cb8153b0657e
FabioVergani <fabio.vergani.1975@gmail.com>
2015-02-28 18:46:43 +0100

    Update json_encode_pretty.php

--- json_encode_pretty.php
+++ json_encode_pretty.php
@@ -1,38 +1,49 @@
-<?php
+ define(space,chr(0x0020));
 
-function json_pretty($json){
- $a = preg_split('|([\{\}\]\[,])|', $json, -1, PREG_SPLIT_DELIM_CAPTURE);
- $r = '';
- $i = 0;
- $t = chr(9);//"\t";
- $n = chr(10);//"\n";
- $l = false;
- foreach($a as $s){
-	if($s == ''){continue;};
-	$p = str_repeat($t,$i);
-	if(!$l && ($s == '{' || $s == '[')){
-	 $i++;if(($r != '') && ($r[(strlen($r) - 1) ] == $n)){$r.= $p;};$r.=($s.$n);
-	}elseif(!$l && ($s == '}' || $s == ']')){
-	 $i--;$p = str_repeat($t,$i);$r.=($n.$p.$s);
-	}elseif(!$l && $s == ','){
-	 $r.=($s.$n);
-	}else{
-	 $r.=(($l ? '' : $p).$s);
-	 if((substr_count($s,'"') - substr_count($s,'\\"')) % 2 != 0){$l = !$l;}
-	};
+ function dump(&$o){
+  $r='';
+  if(!empty($o)){
+   $r='<ul>';
+   foreach($o as $p => $v){
+    $c=(empty($v)?(space.'class="'.$v===0?'zero':'empty'.'"'):'');
+    if(is_array($v)||is_object($v)){
+     $r.=('<ul'.$c.'><strong>'.$p.'</strong>:<div>'.dump($v).'</div></ul>');
+    }else{
+     $r.=('<li'.$c.'><b class="key">'.$p.'</b>:&emsp;'.$v.'</li>');
+    };
+   };
+   $r.='</ul>';
+  };
+  return $r;
  };
- return $r;
-};
-//
 
-function json_encode_pretty($obj){return json_pretty(json_encode($obj));};
-//
+ function json_pretty($json){
+  $r='';
+  if(!empty($json)){
+   $a=preg_split('|([\{\}\]\[,])|', $json, -1, PREG_SPLIT_DELIM_CAPTURE);
+   $i=0;
+   $t=chr(9);//"\t";
+   $n=chr(10);//"\n";
+   $l=false;
+   foreach($a as $s){
+     if($s==''){continue;};
+     $p=str_repeat($t,$i);
+     if(!$l && ($s=='{'||$s=='[')){
+      $i++;if(($r!='') && ($r[(strlen($r) - 1)]==$n)){$r.=$p;};$r.=($s.$n);
+     }elseif(!$l && ($s=='}'||$s==']')){
+      $i--;$p=str_repeat($t,$i);$r.=($n.$p.$s);
+     }elseif(!$l && $s==','){
+      $r.=($s.$n);
+     }else{
+      $r.=(($l?'':$p).$s);
+      if((substr_count($s,'"') - substr_count($s,'\\"')) % 2!=0){$l=!$l;}
+     };
+   };
+   $r.=$n;
+  };
+  return $r;
+ };
 
-function printf_pre($x){return printf('<pre class="print">%s</pre>',$x);};
-//
+ function nicejson($o){return json_pretty(json_encode($o));};
 
-//printf_pre(stripcslashes(json_encode_pretty($xxx)));
-
-
-die('');
-?>
+ function mtx($n,$v){return titled($n,stripcslashes(nicejson($v)));};
